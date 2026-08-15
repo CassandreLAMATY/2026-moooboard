6eb043d6dca890d8b0976f023cc0410fc759ff39
CassandreLAMATY <cassandre.lamaty@gmail.com>
2026-08-15 18:42:39 +0200

    init responsive navbar

---
+++ app/app.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="page-container theme01">
+        <NuxtPage />
+    </div>
+</template>
---
+++ app/components/navbar.vue
@@ -0,0 +1,58 @@
+<template>
+    <nav>
+        <div class="action-container">
+            <button
+                class="btn-round"
+                type="button"
+            >
+                <img
+                    src="/images/arrow-undo.svg"
+                    alt="Undo icon"
+                />
+            </button>
+            <button
+                class="btn-round"
+                type="button"
+            >
+                <img
+                    src="/images/arrow-redo.svg"
+                    alt="Redo icon"
+                />
+            </button>
+        </div>
+
+        <div class="logo-container">
+            <img
+                src="/images/cow-face.svg"
+                alt="MoooBoard Logo"
+            />
+            <span class="font01 micro01">MoooBoard</span>
+        </div>
+
+        <div class="profile-container">
+            <button
+                class="btn-icon-only"
+                type="button"
+            >
+                <img
+                    src="/images/gear.svg"
+                    alt="MoooBoard Logo"
+                />
+            </button>
+            <button
+                class="btn"
+                type="button"
+            >
+                <img
+                    src="/images/door-open-arrow-in.svg"
+                    alt="MoooBoard Logo"
+                />
+                <span class="font02 micro02"> Sign in </span>
+            </button>
+        </div>
+    </nav>
+</template>
+
+<style scoped lang="scss">
+@use '~/assets/scss/navbar.scss';
+</style>
